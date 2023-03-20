45e0cb4eb06f3c8d8bf4109eabddbbde9686e2bc
YAaKkaWa <125532768+hyperAdina@users.noreply.github.com>
2023-03-20 15:08:41 +0000

    code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def webapphw():
-    subjects = ['Subject 1', 'Subject 2', 'Subject 3']
+    subjects = ['Study', 'CCA', 'Life']
     return render_template('webapphw.html', subjects=subjects)
 
 app.run(host='0.0.0.0', port=81)
